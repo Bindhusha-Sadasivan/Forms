3d29c402a694cdb8e12d367fdc129d1278202d4e
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-01-18 10:24:23 +0530

    188 - TD Submitting and Using the Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone:true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:[FormsModule, CommonModule]
 })
 export class AppComponent {
+
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
+
+  // Displays the complete form
+  // onSubmit(form:HTMLFormElement){
+  //   console.log(form);
+  // }
+
+  //Displays the values of the each field we entered
+  onSubmit(form:NgForm){
+      console.log(form);
+    }
 }
